9692f15c2896735d22941c6967939a03a4035483
Onela25 <onelawellem0525@gmail.com>
2024-10-12 09:06:31 +0200

    Added a background image , Added an images foldr with new images . and Edited the h1 size and font in the css.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-alert("Are you ready to cook?");
+alert("I hope you have all your ingridients!Are you ready to cook?");
+
+function generateRecipe(event) {
+    event.preventDefault();
+
+    alert("Generatig recipe... please wait");
+
+    let recipeElement = document.querySelector("#poem");
+    new Typewriter('#typewriter', {
+  strings: ['Hello', 'World'],
+  autoStart: true,
+    recipeElement.innerHTML = "Top-notch choice. 🥑 Just grab some bread, toast it up nice and crisp, then mash up a ripe avocado with a squeeze of lemon juice, salt, and pepper. Spread the avocado mix on your toast and you're good to go. Maybe add a poached egg or some chili flakes if you’re feeling fancy. Enjoy!"
+    
+}
+
+let recipeFormElemnt = document.querySelector("#recipe-generator-form");
+recipeFormElemnt.addEventListener("submit",generateRecipe);
