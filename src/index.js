34f14be6a63da61e31b34d97deadcd805285f06b
Onela25 <onelawellem0525@gmail.com>
2024-10-12 09:29:56 +0200

    Added Basic form handling and typewriter plugin.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-alert("I hope you have all your ingridients!Are you ready to cook?");
+alert("I hope you have all your ingredients! Are you ready to cook?");
 
-function generateRecipe(event) {
-    event.preventDefault();
+        function generateRecipe(event) {
+            event.preventDefault();
 
-    alert("Generatig recipe... please wait");
+            alert("Generating recipe... please wait");
 
-    let recipeElement = document.querySelector("#poem");
-    new Typewriter('#typewriter', {
-  strings: ['Hello', 'World'],
-  autoStart: true,
-    recipeElement.innerHTML = "Top-notch choice. 🥑 Just grab some bread, toast it up nice and crisp, then mash up a ripe avocado with a squeeze of lemon juice, salt, and pepper. Spread the avocado mix on your toast and you're good to go. Maybe add a poached egg or some chili flakes if you’re feeling fancy. Enjoy!"
-    
-}
+            const typewriter = new Typewriter('#recipe', {
+                autoStart: true,
+                delay: 50, 
+                cursor: "",
+            });
 
-let recipeFormElemnt = document.querySelector("#recipe-generator-form");
-recipeFormElemnt.addEventListener("submit",generateRecipe);
+            typewriter
+                .typeString('Top-notch choice. 🥑 Just grab some bread, toast it up nice and crisp, then mash up a ripe avocado with a squeeze of lemon juice, salt, and pepper. Spread the avocado mix on your toast and you are good to go. Maybe add a poached egg or some chili flakes if you’re feeling fancy. Enjoy!')
+                .start(); 
+        }
+
+        let recipeFormElement = document.querySelector("#recipe-generator-form");
+        recipeFormElement.addEventListener("submit", generateRecipe);
